7ee1a0d13c25c0917f25489d18cbbd0c8e76b8c1
ChayaGreisman <chayamgreisman@gmail.com>
2020-07-20 11:39:16 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -5,13 +5,46 @@ import Table from './containers/Table';
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
+
 class App extends Component {
 
+  state = {
+    sushis: [],
+    budget: 100,
+    emptyPlates: []
+    // page: 1
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res=>res.json())
+    .then(sushis=> this.setState({sushis}))
+  }
+
+  deduct = (amount) => {
+    this.setState({budget: this.state.budget-amount})
+  }
+
+
+  newEmptyPlate = () => {
+    this.setState({emptyPlates: [...this.state.emptyPlates, "anotha one"]})
+  }
+  // getSushis = () => {
+  //   fetch(`http://localhost:3000/sushis/?_limit=4&page=${this.state.page}`)
+  //   .then(res=>res.json())
+  //   .then(sushis=> this.setState({sushis}))
+  // }
+
+  // loadMore = () => {
+  //   this.setState({page: this.state.page +1}, this.getSushis())
+  // }
+
   render() {
+    console.log(this.state)
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer budget={this.state.budget} sushis = {this.state.sushis} deduct = {this.deduct} newEmptyPlate={this.newEmptyPlate} />
+        <Table budget={this.state.budget} emptyPlates={this.state.emptyPlates}/>
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,39 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
-const Sushi = (props) => {
-  return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
-          :
-            <img src={/* Give me an image source! */ } width="100%" />
-        }
+class Sushi extends React.Component {
+
+  state = {
+    eaten: false
+  }
+
+  eatSushi = () => {
+    if (this.props.budget >= this.props.price){
+      this.setState(prevState=> {
+        return {eaten: !prevState.eaten}
+      })
+      this.props.deduct(this.props.price)
+      this.props.newEmptyPlate()
+    }
+  }
+
+  render(){
+    return (
+      <div className="sushi">
+        <div className="plate" 
+            onClick={this.eatSushi}>
+          { 
+            this.state.eaten
+            ?null
+            :<img src={this.props.img_url} width="100%" />
+          }
+        </div>
+        <h4 className="sushi-details">
+          {this.props.name} - ${this.props.price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
-  )
+    )
+  }
+
 }
 
 export default Sushi
