25aab8159ead402b101c286b6cb7ea23407b8844
chloekim917 <chloekim917@gmail.com>
2020-07-20 11:34:15 -0400

    trying to implement more sushi

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -2,18 +2,37 @@ import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
 import Sushi from '../components/Sushi'
 
+class SushiContainer extends React.Component {
+  state={
+    sushis: []
+  }
+  more=()=>{
+    this.setState({sushis:this.props.})
+  }
 
-const SushiContainer = (props) => {
+  render(){  
     return (
       <Fragment>
         <div className="belt">
-          {props.sushis.slice(0,4).map(sushi=> <Sushi key={sushi.id} {...sushi}/>)}
+          {this.props.sushis.slice(0,4).map(sushi=> <Sushi key={sushi.id} {...sushi}/>)}
           <MoreButton />
           
         </div>
       </Fragment>
-    )
-  
+    )}
+
 }
+// const SushiContainer = (props) => {
+//     return (
+//       <Fragment>
+//         <div className="belt">
+//           {props.sushis.slice(0,4).map(sushi=> <Sushi key={sushi.id} {...sushi}/>)}
+//           <MoreButton more={more}/>
+          
+//         </div>
+//       </Fragment>
+//     )
+  
+// }
 
 export default SushiContainer
