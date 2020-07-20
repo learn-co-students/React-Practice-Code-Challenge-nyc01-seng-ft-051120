2b870db2d554c15ccd3dfcac2ee986ebb9746561
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-07-20 12:20:31 -0400

    forgot to push sorry

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -4,17 +4,17 @@ const Sushi = (props) => {
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
+           onClick={()=>props.eatMe(props.id)}>
         { 
           /* Tell me if this sushi has been eaten! */ 
           false ?
             null
           :
-            <img src={/* Give me an image source! */ } width="100%" />
+            <img src={props.img_url } width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {props.name} - ${props.price}
       </h4>
     </div>
   )
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import MoreButton from '../components/MoreButton'
-// import Sushi from '../components/Sushi'
+import Sushi from '../components/Sushi'
 
 class SushiContainer extends React.Component {
 
@@ -8,18 +8,36 @@ class SushiContainer extends React.Component {
     limit:4
   }
 handleMoreSushi=()=>{
+if(this.state.limit < this.props.sushis){
+  this.setState((prevState) => ({ limit: (prevState.limit + 4) }));
+}
+}
+
+eatMe = (id)=>{
   
+  let eat = this.props.sushis.filter(i=>i!==id)
+  console.log(eat)
+
 }
 
   render() {
+    let sushis=this.props.sushis.slice(0,this.state.limit)
     return (
       
          <React.Fragment>
       <div className="belt">
-        {
-          // <Sushi />
+       
+        {sushis.map(sushi=>
+        <Sushi 
+        eatMe={this.eatMe}
+        id={sushi.id}
+        name={sushi.name}
+      img_url={sushi.img_url}
+     price={sushi.price}
+        />
+        )
         }
-        <MoreButton />
+        <MoreButton handleMoreSushi={this.handleMoreSushi}/>
       </div>
     </React.Fragment>
   ) 
