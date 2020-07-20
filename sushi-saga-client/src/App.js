e98c4e04240be592a79b17078cd02687ba1144c9
Gary Cordero <GaryCordero1690@gmail.com>
2020-07-20 10:43:12 -0400

    displaying 4 sushies

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,11 +6,30 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  
+  state = {
+    sushies:[],
+    currentIndex:0
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(resp => resp.json())
+    .then(sushies => this.setState({sushies}))
+  }
+
+  increaseIndex = ()=>{
+    this.setState({currentIndex: this.state.currentIndex + 4})
+  }
 
   render() {
+    
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer  
+          sushies={this.state.sushies} 
+          currentIndex={this.state.currentIndex}
+          increaseIndex={this.increaseIndex}/>
         <Table />
       </div>
     );
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,29 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
+ 
+function displayNextFourSushies(sushies,currentIndex){
+    let sushiesToBeDisplay = []
+    for (let i = currentIndex; i < currentIndex + 4 ; i++){
+      sushiesToBeDisplay.push(<Sushi {...sushies[i]}/>)
+    }
+    return sushiesToBeDisplay
+}
+
+
 
 const SushiContainer = (props) => {
+  console.log(props)
   return (
     <Fragment>
       <div className="belt">
         {
+          displayNextFourSushies(props.sushies,props.currentIndex)
           /* 
              Render Sushi components here!
           */
         }
-        <MoreButton />
+        <MoreButton increaseIndex={props.increaseIndex}/>
       </div>
     </Fragment>
   )
